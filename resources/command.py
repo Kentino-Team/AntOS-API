33bd29e7b2060e3caca097ee7f7730f4728386a3
Clément Vicart <clem.vicart@gmail.com>
2021-11-22 17:14:39 +0100

    added new ui and commands security

--- resources/command.py
+++ resources/command.py
@@ -1,8 +1,10 @@
+import json
+from bson import json_util
 from flask_restful import Resource
 from config.db import db
 from flask import request
 from flask_cors import cross_origin, CORS
-from flask_jwt import jwt_required
+from flask_jwt import jwt_required, current_identity
 
 
 class Command(Resource):
@@ -10,6 +12,13 @@ class Command(Resource):
     @jwt_required()
     def post(self, rig=""):
         command = request.json['command']
+
+        rig_owner = db.farms.find_one({
+            "workers.id": int(rig)
+        })
+        rig_owner = json.loads(json_util.dumps(rig_owner))
+        if rig_owner['user_id'] != current_identity.id:
+            return {"error": "forbidden"}, 403
 
         db.commands.insert_one({
             "rig_id": int(rig),
