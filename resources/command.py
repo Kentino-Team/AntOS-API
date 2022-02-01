5560bdef07ca85412a7ead4e6682a7d118e59bdc
Clément Vicart <clem.vicart@gmail.com>
2022-02-01 09:42:18 +0100

    changing db url to local database

--- resources/command.py
+++ resources/command.py
@@ -1,10 +1,13 @@
 import json
+
+import pymongo
 from bson import json_util
 from flask_restful import Resource
 from config.db import db
 from flask import request
 from flask_cors import cross_origin, CORS
 from flask_jwt import jwt_required, current_identity
+from datetime import datetime
 
 
 class Command(Resource):
@@ -21,7 +24,8 @@ class Command(Resource):
             return {"error": "forbidden"}, 403
 
         base_command = {
-            "rig_id": int(rig)
+            "rig_id": int(rig),
+            "timestamp": datetime.now()
         }
 
         if command == "batch":
@@ -46,6 +50,21 @@ class Command(Resource):
 
         db.commands.insert_one(base_command)
         return 200
+
+    @jwt_required()
+    def get(self, rig=""):
+        rig_owner = db.farms.find_one({
+            "workers.id": int(rig)
+        })
+        rig_owner = json.loads(json_util.dumps(rig_owner))
+        if rig_owner['user_id'] != current_identity.id:
+            return {"error": "forbidden"}, 403
+
+        commands = db.commands.find({
+            "rig_id": int(rig)
+        }).sort([("timestamp", pymongo.DESCENDING)]).limit(10)
+        commands = json.loads(json_util.dumps(commands))
+        return commands, 200
 
     @cross_origin()
     def options(self):
